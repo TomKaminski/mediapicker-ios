082fa830cf2fb7802613d016b8ebc8044133c6ba
Tomasz Kaminski <>
2019-12-06 10:24:14 +0100

    removed live photo

--- MediaPicker/Library/LibraryAlbum.swift
+++ MediaPicker/Library/LibraryAlbum.swift
@@ -19,7 +19,7 @@ class Album {
     
     let itemsFetchResult = PHAsset.fetchAssets(in: collection, options: Utils.fetchOptions())
     itemsFetchResult.enumerateObjects({ (asset, count, stop) in
-      if asset.mediaType == .image {
+      if asset.mediaType == .image && !asset.mediaSubtypes.contains(.photoLive) {
         self.images.append(Image(asset: asset, guid: UUID().uuidString, newlyTaken: false, customFileName: FileNameComposer.getImageFileName(), dateAdded: Date()))
       } else if asset.mediaType == .video {
         self.videos.append(Video(asset: asset, guid: UUID().uuidString, customFileName: FileNameComposer.getImageFileName(), newlyTaken: false, dateAdded: Date()))
